a0b9784f3528e7744a26e82f4853e3158925c416
milovanderlinden <milo@dogodigi.net>
2014-10-10 23:41:12 +0200

    preparing to implement unit testing

--- arcgis-rest-batch.js
+++ arcgis-rest-batch.js
@@ -1,20 +1,20 @@
 /**
  *  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
  * 
- *  This file is part of arcgisminer
+ *  This file is part of arcgis-rest-cli
  *  
- *  arcgisminer is free software: you can redistribute it and/or modify
+ *  arcgis-rest-cli is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  arcgisminer is distributed in the hope that it will be useful,
+ *  arcgis-rest-cli is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with arcgisminer. If not, see <http://www.gnu.org/licenses/>.
+ *  along with arcgis-rest-cli. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 'use strict';
--- config/config.sample.js
+++ config/config.sample.js
@@ -11,11 +11,11 @@ module.exports = {
     useRootServices: true,
     outputdir: 'data/arcgisonline',
     useTor: true, // You can use a Tor proxy running on localhost if you want to.
-    useLayerList: false, //Do you want to use a layers list to limit the datasets parsed?
-    useServicesList: false, //Do you want to use a serviceslist to servicesthat are being checked?
+    useLayerList: true, //Do you want to use a layers list to limit the datasets parsed?
+    useServicesList: true, //Do you want to use a serviceslist to servicesthat are being checked?
     useFolderList: false, //Do you want to use a serviceslist to servicesthat are being checked?
-    folderList: ['somefolder'],
-    servicesList: ['somefolder/somesubfolder'], //Provide an array of services that you want parsed assuming you know their correct name.
+    folderList: ['Demographics'],
+    servicesList: ['Demographics/USA_Average_Household_Size'], //Provide an array of services that you want parsed assuming you know their correct name.
     layersList: [2, 12, 14, 23], // Provide a list of layer id's (integers) //[8,9];//,45,29,30,31,34,39,41,32,27];
     offsetStart: 0, //the lower limit of the OBJECTID. Can be changed if certain datasets halted somewhere in the middle during processing (start at this OBJECTID)
     testOnly: true, //run tests and log output. Don't parse and store data
