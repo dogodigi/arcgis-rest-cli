893becf785138568295b0518230892445efb0b09
milovanderlinden <milo@dogodigi.net>
2014-10-10 23:43:17 +0200

    commented in config.sample.js

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,5 +1,6 @@
 /**
- * Settings
+ * This is an example configuration file for running arcgis-rest-cli.
+ * If no config is given as parameter, this config file will be ran.
  */
 module.exports = {
     options: {
@@ -16,7 +17,7 @@ module.exports = {
     useFolderList: false, //Do you want to use a serviceslist to servicesthat are being checked?
     folderList: ['Demographics'],
     servicesList: ['Demographics/USA_Average_Household_Size'], //Provide an array of services that you want parsed assuming you know their correct name.
-    layersList: [2, 12, 14, 23], // Provide a list of layer id's (integers) //[8,9];//,45,29,30,31,34,39,41,32,27];
+    layersList: [2, 12, 14, 23], // Provide a list of layer id's (integers)
     offsetStart: 0, //the lower limit of the OBJECTID. Can be changed if certain datasets halted somewhere in the middle during processing (start at this OBJECTID)
     testOnly: true, //run tests and log output. Don't parse and store data
     debug: true
