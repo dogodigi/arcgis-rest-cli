d9324cd88d32842c92b00f64f0cac281494168b6
milovanderlinden <milo@dogodigi.net>
2014-12-01 22:17:07 +0100

    npm start and npm test operational

--- test/test-arcgis-rest.js
+++ test/test-arcgis-rest.js
@@ -1,27 +1,51 @@
+/**
+ *  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
+ * 
+ *  This file is part of arcgis-rest-cli
+ *  
+ *  arcgis-rest-cli is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  arcgis-rest-cli is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with arcgis-rest-cli. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+'use strict';
 var arcgisRest = require('../lib/arcgis-rest.js');
 arcgisRest.config = {};
 
 exports.testHelperFunctions = {
-    testIntersect: function (test) {
+    intersect: function (test) {
         test.deepEqual(arcgisRest.intersect(['12', '2', 'hi'], ['2', '3', 'boo']), ['2']);
         test.done();
     },
-    testLogger: function (test) {
+    logger: function (test) {
         test.equal(arcgisRest.logger('info', 'this is a message'), '[info] this is a message\n');
         test.done();
     },
-    testMkdirSync: function (test) {
+    mkdirSync: function (test) {
         var fs = require('fs');
         test.equals(arcgisRest.mkdirSync('test'), false, 'Directory test does not exists');
         test.equals(arcgisRest.mkdirSync('boo'), true, 'Directory boo could not be created');
         fs.rmdirSync('boo');
+        test.done();
+    },
+    mkdirpSync: function (test) {
+        var fs = require('fs');
         test.equals(arcgisRest.mkdirpSync('boo/foo'), true, 'Directory boo/foo could not be created');
         fs.rmdirSync('boo/foo');
         fs.rmdirSync('boo');
         test.done();
     }
 };
-exports.testArcgis = {
+exports.testArcGisRest = {
     setUp: function (callback) {
         arcgisRest.config.options = {
             socksPort: 9050, // Tor port for local tor instance
@@ -37,37 +61,51 @@ exports.testArcgis = {
         };
         callback();
     },
-    testVersionGet: function (test) {
-        arcgisRest.retrieve('/ArcGIS/rest/services/?f=json&pretty=true', '', function (result) {
+    get: function (test) {
+        arcgisRest.get('/ArcGIS/rest/services/?f=json&pretty=true', '', function (result) {
             test.equals(result.currentVersion, '10.2', 'arcgis server is no longer running version 10.2');
             test.done();
         });
     },
-    testVersionPost: function (test) {
-        arcgisRest.postRetrieve('/ArcGIS/rest/services', {"f": "json", "pretty": "true"}, '', function (result) {
+    post: function (test) {
+        arcgisRest.post('/ArcGIS/rest/services', {"f": "json", "pretty": "true"}, '', function (result) {
             test.equals(result.currentVersion, '10.2', 'arcgis server is no longer running version 10.2');
             test.done();
         });
     },
-    testGetIdfield: function (test) {
+    getIdfield: function (test) {
         arcgisRest.getIdfield({"name": "Demographics/USA_Average_Household_Size"}, {"id": 1},
         function (result) {
             test.equals(result, 'OBJECTID', 'Demographics/USA_Average_Household_Size does not have OBJECTID as ID field');
             test.done();
         });
     },
-    testGetObjectIds: function (test) {
+    getObjectIds: function (test) {
         arcgisRest.getObjectIds({"name": "Demographics/USA_Average_Household_Size"}, {"id": 4},
         function (result) {
             test.equals(result.objectIds.length, '52', 'Demographics/USA_Average_Household_Size does not have 52 objects for States');
             test.done();
         });
     },
-    testProcessLists: function (test) {
-        arcgisRest.processLists(
+    getLists: function (test) {
+        arcgisRest.getLists(
                 function (result) {
                     test.equals(result.services.length, '12', 'server.arcgisonline.com does not have 12 services');
                     test.equals(result.folders.length, '7', 'server.arcgisonline.com does not have 7 folders');
+                    test.done();
+                });
+    },
+    getParseResult: function (test) {
+        arcgisRest.parseResult(0,1,{"name": "Demographics/USA_Average_Household_Size"}, {"id": 4},'OBJECTID', 'esriGeometryPolygon',
+                function (result) {
+                    test.equals(result.error.code, 400, 'server.arcgisonline.com did return features while an error was expected');
+                    test.done();
+                });
+    }, 
+    postParseResult: function (test) {
+        arcgisRest.parseResult(0,1,{"name": "Demographics/USA_Average_Household_Size"}, {"id": 4},{"objectIdFieldName":"OBJECTID","objectIds":[1]}, 'esriGeometryPolygon',
+                function (result) {
+                    test.equals(result.features.length, 1, 'server.arcgisonline.com did not return feature');
                     test.done();
                 });
     }
